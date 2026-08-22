8bb7dfdfed4d9e1e5563858f86a07510af2a800f
PuglieseJoaquin <pugliesejoaquinsebastian214@gmail.com>
2026-08-22 20:05:12 -0300

    Agregue los mails del grupo

--- src/Threes/Principal.java
+++ src/Threes/Principal.java
@@ -3,9 +3,12 @@ package Threes;
 public class Principal {
 
 	public static void main(String[] args) {
-		/* Grupo N°5
-		. Joaquin Pugliese: pugliesejoaquinsebastian214@gmail.com
-		
+		/* GRUPO N°5
+		. Joa: pugliesejoaquinsebastian214@gmail.com
+		mial: 
+		. Julian: julianponceflores93@gmail.com
+		. Juan: juanspereyra@live.com
+		. Rami: ramisalv777@gmail.com
 		 */
 
 	}
