db662a67b1c7b1a75997f6d320a458e362d2a845
PuglieseJoaquin <pugliesejoaquinsebastian214@gmail.com>
2026-08-22 19:59:17 -0300

    Primer commit del admin. Joa

---
+++ src/Threes/Principal.java
@@ -0,0 +1,13 @@
+package Threes;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		/* Grupo N°5
+		. Joaquin Pugliese: pugliesejoaquinsebastian214@gmail.com
+		
+		 */
+
+	}
+
+}
